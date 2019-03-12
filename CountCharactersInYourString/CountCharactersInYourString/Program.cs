c0095117dcf6af267cf90c731559cdb7305fe82f
Dariusz Jagiellowicz <darekjagiellowicz@gmail.com>
2019-03-12 21:47:59 +0100

    Added count characters in your string challenge

---
+++ CountCharactersInYourString/CountCharactersInYourString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountCharactersInYourString
+{
+    // https://www.codewars.com/kata/count-characters-in-your-string
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static Dictionary<char, int> Count(string str)
+        {
+            return str.GroupBy(character => character)
+                      .ToDictionary(character => character.Key, value => value.Count());
+        }
+    }
+}
